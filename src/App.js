e3a8aa39bf2398feb55ac5c9e0d96803c215ebe8
BlakeACollins <Blakeac369@gmail.com>
2024-06-15 15:01:09 -0700

    react commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { useEffect } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Navbar from './Components/Navbar/Navbar';
+import Landing_Page from './Components/Landing_Page/Landing_Page';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+       <BrowserRouter>
+          <Navbar/>
+              <Routes>
+              <Route path="/" element={<Landing_Page/>}/>
+              </Routes>
+            
+        </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
